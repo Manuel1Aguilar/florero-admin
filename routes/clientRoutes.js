78d369b80e6ef6f5ad4787bd73360777b634226b
Manuel Aguilar <manuelmla.aguilar@gmail.com>
2024-10-22 17:24:36 -0300

    Add console logs to client errs

--- routes/clientRoutes.js
+++ routes/clientRoutes.js
@@ -17,6 +17,7 @@ router.get('/', (_, res) => {
 router.get('/client', (_, res) => {
     db.all('SELECT * FROM colors WHERE stock > 0 AND deleted = 0', [], (err, colors) => {
         if (err) {
+            console.error(err);
             res.status(500).send('Error fetching colors');
         } else {
             res.render('client', { colors });
@@ -41,6 +42,7 @@ router.post('/save-order', (req, res) => {
     const query = `INSERT INTO orders (client_name, order_code, color_spec) VALUES (?, ?, ?);`;
     db.run(query, [client_name, order_code, colorSpecString], function (err) {
         if (err) {
+            console.error(err);
             res.status(500).send('Error saving order.');
         } else {
             res.render('order_confirmation', { client_name, order_code });
